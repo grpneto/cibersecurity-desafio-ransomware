11b0d53e4853c8132d9ca8f7ca2a738d9d0978ac
Gildarte Ramos <gildarteneto9@gmail.com>
2024-12-10 15:17:19 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,21 +1,25 @@
+## Comando usado para importar a biblioteca de criptografia
 import os
 import pyaes
 
-## abrir o arquivo criptografado
+## Comando usado para abrir o arquivo criptografado
 file_name = "teste.txt.ransomwaretroll"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## chave para descriptografia
+## Chave de 16 bytes usada para descriptografia
 key = b"testeransomwares"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
+## O arquivo criptografado é removido, mas o seu conteúdo permanece
 os.remove(file_name)
 
-## criar o arquivo descriptografado
+"""
+O arquivo descriptografado é criado. A descriptografia só será feita com o uso do comando
+decrypter.py
+"""
 new_file = "teste.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
